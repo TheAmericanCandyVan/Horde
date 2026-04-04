6c9ef3f04dc2a0d40913c89288476b344a4d149f
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-04-04 12:32:25 -0700

    improve styling

--- gamemodes/horde/gamemode/arccw/attachments/horde_go_perk_bandito.lua
+++ gamemodes/horde/gamemode/arccw/attachments/horde_go_perk_bandito.lua
@@ -5,7 +5,7 @@ AddCSLuaFile()
 local att = {}
 
 att.PrintName = "Bandito"
-att.Icon = Material("entities/acwatt_go_perk_cowboy.png", "mips smooth")
+att.Icon = Material( "entities/acwatt_go_perk_cowboy.png", "mips smooth" )
 att.Description = "show off your inner pistolero at the cost of extra recoil, but hey atleast you look cool, lets you shoot while sprinting"
 att.Desc_Pros = {}
 att.Desc_Cons = {}
@@ -17,15 +17,16 @@ att.Mult_Recoil = 1.25
 att.LHIK = true
 att.LHIKHide = true
 
-att.Hook_ShouldNotSight = function(wep)
+att.Hook_ShouldNotSight = function( wep )
     local notSprinting
 
-    if  wep:GetState() == ArcCW.STATE_SPRINT then
+    if wep:GetState() == ArcCW.STATE_SPRINT then
         notSprinting = true
     elseif wep:GetState() ~= ArcCW.STATE_SPRINT then
         notSprinting = false
     end
+
     return notSprinting
 end
 
-ArcCW.LoadAttachmentType(att, "horde_go_perk_bandito")
+ArcCW.LoadAttachmentType( att, "horde_go_perk_bandito" )
--- gamemodes/horde/gamemode/arccw/attachments/horde_m249_12g_belt.lua
+++ gamemodes/horde/gamemode/arccw/attachments/horde_m249_12g_belt.lua
@@ -1,15 +1,14 @@
 if not ArcCWInstalled then return end
 
 AddCSLuaFile()
+
 local att = {}
 
 att.PrintName = "45 Round 12G Belt"
-att.Icon = Material("entities/acwatt_go_m249_mag_12g_45.png", "mips smooth")
+att.Icon = Material( "entities/acwatt_go_m249_mag_12g_45.png", "mips smooth" )
 att.Description = "Conversion for the M249, turning it into a 12 gauge automatic shotgun."
-att.Desc_Pros = {
-}
-att.Desc_Cons = {
-}
+att.Desc_Pros = {}
+att.Desc_Cons = {}
 att.SortOrder = 40
 att.AutoStats = true
 att.Slot = "go_m249_mag"
@@ -41,7 +40,7 @@ att.Override_ShellScale = 2
 
 att.Override_Trivia_Class = "Machine Shotgun"
 att.Override_Trivia_Calibre = "12 Gauge"
-att.ActivateElements = {"go_m249_mag_12g_45"}
+att.ActivateElements = { "go_m249_mag_12g_45" }
 
 att.Override_BulletBones = {
     [1] = "v_weapon.bullet11",
@@ -52,8 +51,9 @@ att.Override_BulletBones = {
     [6] = "v_weapon.bullet01",
 }
 
-att.Hook_GetShootSound = function(wep, fsound)
+att.Hook_GetShootSound = function( _, fsound )
     if fsound == "arccw_go/m249/m249-1.wav" then return "arccw_go/nova/nova-1.wav" end
     if fsound == "arccw_go/m4a1/m4a1_silencer_01.wav" then return "arccw_go/m590_suppressed_fp.wav" end
 end
-ArcCW.LoadAttachmentType(att, "horde_m249_12g_belt")
+
+ArcCW.LoadAttachmentType( att, "horde_m249_12g_belt" )
